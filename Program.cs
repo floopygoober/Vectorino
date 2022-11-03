6656e54079b1265c2638a8c1bd1c1e783b2b7ffa
Benjamin Glase <benglase89@gmail.com>
2022-11-03 14:02:16 -0700

    Add files via upload
    These are the classes with code in them

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace vectorino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char retry;
+            do
+            {
+                Console.Clear();
+                double newX, newY;
+                //sets both x and y values
+                Console.WriteLine("Enter an x value for your vector");
+                newX = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter an y value for your vector");
+                newY = Convert.ToDouble(Console.ReadLine());
+
+                //building our first 2d vector
+                Vector2D myVector = new Vector2D(newX, newY);
+                Console.WriteLine("[" + myVector.X + ", " + " " + myVector.Y + "]");
+                //this will keep the console window open when the program is finished until next keystroke
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("Press 1 to open simple menu");
+                Console.WriteLine("Press 2 to open advanced menu");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                char input = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                //switch statement to sort through all the options following the order of the printed menu above
+                switch (input)
+                {
+                    case '1':
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine("Press 1 to negate X");
+                        Console.WriteLine("Press 2 to negate Y");
+                        Console.WriteLine("Press 3 to add vectors");
+                        Console.WriteLine("Press 4 to subtract vectors");
+                        Console.WriteLine("Press 5 to perform scalar multiplication");
+                        Console.WriteLine("Press 6 to get the magnitude");
+                        Console.WriteLine("Press 7 to get the angle in degrees");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        char input1 = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+                        switch (input1)
+                        {
+                            case '1':
+                                myVector.NegateX();
+                                Console.WriteLine(newX + " is now: " + myVector.X);
+                                break;
+
+                            case '2':
+                                myVector.NegateY();
+                                Console.WriteLine(newY + " is now: " + myVector.Y);
+                                break;
+
+                            case '3':
+                                Console.WriteLine("What would you like to add to your X value");
+                                double addX = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine("What would you like to add to your Y value");
+                                double addY = Convert.ToDouble(Console.ReadLine());
+                                myVector.AddVector(addX, addY);
+                                Console.WriteLine("your x value is now: " + myVector.X + " and your y is now: " + myVector.Y);
+                                break;
+
+                            case '4':
+                                Console.WriteLine("What would you like to subtract from your X value");
+                                double subX = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine("What would you like to subtract from your Y value");
+                                double subY = Convert.ToDouble(Console.ReadLine());
+                                myVector.SubtractVector(subX, subY);
+                                Console.WriteLine("your x value is now: " + myVector.X + " and your y is now: " + myVector.Y);
+                                break;
+
+                            case '5':
+                                Console.WriteLine("What would you like to multiply your vector by? ");
+                                double mult = Convert.ToDouble(Console.ReadLine());
+                                myVector.ScalerMultiplication(mult);
+                                Console.WriteLine("your x value is now: " + myVector.X + " and your y is now: " + myVector.Y);
+                                break;
+
+                            case '6':
+                                Console.WriteLine("The magnitude is: " + myVector.GetMagnitude());
+                                break;
+
+                            case '7':
+                                Console.WriteLine("The angle of the vector is: " + myVector.GetAngleInDegrees());
+                                break;
+                        }
+                        
+                        //the end of assignment 1
+                        break;
+
+                    case '2':
+                        Console.WriteLine("Enter an x value for your second vector");
+                        double otherX = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter an y value for your second vector");
+                        double otherY = Convert.ToDouble(Console.ReadLine());
+                        //creats second vector to maths with
+                        Vector2D otherVector = new Vector2D(otherX, otherY);
+
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine("Press 1 to normalize the vector");
+                        Console.WriteLine("Press 2 to get the dot product of the vector");
+                        Console.WriteLine("Press 3 to get the angle between the vectors");
+                        Console.WriteLine("Press 4 to get the LERP of the vector");
+                        Console.WriteLine("Press 5 to rotate the vectore");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        //The new menu for the second assignment
+                        //now for the case section to sort out which opporation is carried out. 
+                        char input2 = Console.ReadKey().KeyChar;
+                        Console.WriteLine();
+                        switch (input2)
+                        {
+                            case '1':
+                                Console.WriteLine("The vector is normalized");
+                                break;
+
+                            case '2':
+                                Console.WriteLine("The result of your DOT product is: " + myVector.GetDotProduct(otherVector));
+                                break;
+
+                            case '3':
+                                Console.WriteLine("The angle between your vectors is: " + myVector.GetAngleBetween(otherVector));
+                                break;
+
+                            case '4':
+                                myVector.Lerp(otherVector);
+                                Console.WriteLine("your new vector is: [" + myVector.X + ", " + myVector.Y + "]");
+                                break;
+
+                            case '5':
+                                Console.WriteLine("how many degrees would you like to rotate this vector?");
+                                double angle = Convert.ToDouble(Console.ReadLine());
+                                myVector.RotateVector(angle);
+                                Console.WriteLine("The vector after the rotation is: [" + myVector.X + ", " + myVector.Y + "]");
+                                break;
+                        }
+
+                        break;
+                }
+
+                Console.WriteLine("would you like to retry? (n or q to quit) ");
+                retry = Char.ToLower(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+
+            }
+            while (retry != 'q' && retry != 'n');
+        }
+    }
+}
---
+++ Vector2D.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectorino
+{
+    class Vector2D
+    {
+        private double x;
+        private double y;
+        //lets us have access to the value of our private doubles with out changing them
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        //same here for our Y double
+        public double Y 
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        //constructor
+        public Vector2D(double xValue, double yValue)
+        {//sets the private doubles to the passed in parameters
+            x = xValue;
+            y = yValue;
+        }
+        //makes X negative
+        public void NegateX()
+        {
+            x = -x;
+        }
+        //makes Y negative
+        public void NegateY()
+        {
+            y = -y;
+        }
+        //used to add user entered doubles with the origenal doubles
+        public void AddVector(double xValue, double yValue)
+        {
+            x += xValue;
+            y += yValue;
+        }
+        //user entered subtraction
+        public void SubtractVector(double xValue, double yValue)
+        {
+            x -= xValue;
+            y -= yValue;
+        }
+        //user entered multiplication with a scalar double
+        public void ScalerMultiplication(double scalar)
+        {
+            x *= scalar;
+            y *= scalar;
+        }
+        //gets the over all magnitude of the vector
+        public double GetMagnitude()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));            
+        }
+        //used to get the degree of the vector
+        public double GetAngleInDegrees()
+        {
+            return ((Math.Atan2(y, x)) * (180 / Math.PI) + 360);
+        }
+        /*
+         *THIS IS THE END  
+         *OF ASSIGNMENT 2
+         */
+        public void Normalize()
+        {//tests to see if it is normalized
+            double normal = GetMagnitude();
+            x /= normal;
+            y /= normal;
+        }
+
+        public double GetDotProduct(Vector2D otherVector)
+        {//uses quick maths to get and return the dot product
+            return (x * otherVector.X) + (y * otherVector.Y);
+        }
+
+        public double GetAngleBetween(Vector2D otherVector)
+        {
+            //declaring doubles to store each magnitude for easier coding of the angle equation
+            double mag1= Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            double mag2 = Math.Sqrt(Math.Pow(otherVector.X, 2) + Math.Pow(otherVector.Y, 2));
+
+            return (Math.Acos(((x * otherVector.X) + (y * otherVector.Y)) / (mag1 * mag2))) * (180 / Math.PI);
+        }
+
+        public Vector2D Lerp(Vector2D otherVector)
+        {// specifies what the t value should be with a fail safe as an if loop to idiot proof
+            Console.WriteLine("what would you like you t value to be for this lerp? (any value from: 0 -> 1) ");
+            double t = Convert.ToDouble(Console.ReadLine());
+            if (t < 0 || t > 1)
+            {
+                Console.WriteLine("The value must be between 0 and 1. Try again.");
+                t = Convert.ToDouble(Console.ReadLine());
+            }
+            else
+            {
+                double tempx = otherVector.X - x;
+                double tempy = otherVector.Y - y;
+                //finds the difference between the vectors then it scales them by t
+                X += tempx * t;
+                Y += tempy * t;
+            }
+                return this; // returns the entire method
+        }
+
+        public void RotateVector(double angle)
+        {//we change the angle before hand so we do not have to do this with each equation to get the answer in degrees
+            angle /= (180 / Math.PI);
+            double tempX = X;
+            double tempY = Y;
+
+            X = (tempX * Math.Cos(angle)) - (tempY * Math.Sin(angle));
+            Y = (tempX * Math.Sin(angle))  + (tempY * (Math.Cos(angle)));
+        }
+
+    }
+}
